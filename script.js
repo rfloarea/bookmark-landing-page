5fd80e0d02d397dea6af3094300cff93a578095e
rfloarea <account@rfloarea.net>
2024-09-02 18:25:29 -0400

    fix: begin restructure to use localStorage

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 console.log("connected")
 
-// hardcoded elements
 const urlInput = document.querySelector('#url-input');
 const addBookmarkBtn = document.querySelector('#add-bookmark');
 const deleteAllBookmarksBtn = document.querySelector('#delete-all');
 const bookmarkList = document.querySelector('#bookmark-list');
 
-// give them actions
 addBookmarkBtn.addEventListener('click', addBookmark);
 deleteAllBookmarksBtn.addEventListener('click', deleteAllBookmarks);
 
-// callbacks for hardcoded elements
 function addBookmark() {
   if (!urlInput.value) {
     return
@@ -28,84 +25,36 @@ function deleteAllBookmarks() {
   localStorage.clear();
 };
 
-// fun stuff!
 function createNewBookmark() {
+  // get url to play with
   const url = urlInput.value;
-
   // parent li element
   const newBookmark = document.createElement('li');
   newBookmark.setAttribute('class', 'bookmark')
   bookmarkList.appendChild(newBookmark);
-  
-  // child p element
-  const bookmarkLink = document.createElement('p');
-  bookmarkLink.textContent = `${url}`;
-  newBookmark.appendChild(bookmarkLink);
-
-  // child button group element
-  createBookmarkButtonGroup(newBookmark, url);
-
-  // TODO: add to local storage
-  // storage test stuff!!
-  // what if we just create a new key for each url,
-  // rather than creating an array object
-
-  setNewItem(url);
-  
-  
+  newBookmark.innerHTML = 
+  `
+    <a href="${url}" target="_blank" class="url">${url}</a>
+    <div class="button-group">
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  `
+  // clear input
   urlInput.value = '';
 };
 
-localStorage.setItem('counter', JSON.stringify(0));
-
-function setNewItem(url) {
-  let counter = JSON.parse(localStorage.getItem('counter'));
-  console.log(counter);
-  counter = counter + 1;
-  console.log(counter);
-  localStorage.setItem('url'+`${counter}`, JSON.stringify(`${url}`));
-  localStorage.setItem('counter', JSON.stringify(counter));
+// TODO
+function handleEditBookmark() {
+  // access correct item from storage
+  // edit it
+  // put it back
+  // update list
 };
 
-// callbacks for dynamic elements
-function handleGoToLink(url) {
-  console.log(url, ' clicked');
-  window.open(url, '_blank');
+// TODO
+function handleDeleteBookmark() {
+  // find correct item
+  // remove it: localStorage.removeItem(key)
+  // update list of bookmarks
 };
-function handleEditBookmark(newBookmark) {
-  console.log(newBookmark, ' edited');
-  const url = prompt('Edit the url');
-  newBookmark.textContent = url;
-
-  createBookmarkButtonGroup(newBookmark, url)
-};
-function handleDeleteBookmark(newBookmark) {
-  console.log(newBookmark, ' deleted')
-  newBookmark.remove()
-};
-
-// well named function :P
-function createBookmarkButtonGroup(newBookmark, url) {
-
-  // give them names
-  const buttonGroupDiv = document.createElement('div');
-  const goToBookmarkBtn = document.createElement('button');
-  const editBookmarkBtn = document.createElement('button');
-  const deleteBookmarkBtn = document.createElement('button');
-
-  // give them words
-  goToBookmarkBtn.textContent = 'Go to link';
-  editBookmarkBtn.textContent = 'Edit';
-  deleteBookmarkBtn.textContent = 'Delete';
-
-  // give them a home
-  newBookmark.appendChild(buttonGroupDiv);
-  buttonGroupDiv.appendChild(goToBookmarkBtn);
-  buttonGroupDiv.appendChild(editBookmarkBtn);
-  buttonGroupDiv.appendChild(deleteBookmarkBtn);
-
-  // give them life
-  goToBookmarkBtn.addEventListener('click', () => handleGoToLink(url));
-  editBookmarkBtn.addEventListener('click', () => handleEditBookmark(newBookmark));
-  deleteBookmarkBtn.addEventListener('click', () => handleDeleteBookmark(newBookmark));
-}
